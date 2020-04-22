e4e7874f23ce8061f996ae1023ec639b1edbaeb5
Ivan Jacob Agaloos Pesigan <learn.jeksterslab@gmail.com>
2020-04-22 18:49:30 +0800

    Added test_term_renviron.R.

---
+++ tests/testthat/test_term_renviron.R
@@ -0,0 +1,70 @@
+#' ---
+#' title: "Test: term_renviron"
+#' author: "Ivan Jacob Agaloos Pesigan"
+#' date: "`r Sys.Date()`"
+#' output:
+#'   rmarkdown::github_document:
+#'     toc: true
+#' ---
+#'
+#+ setup
+library(testthat)
+library(jeksterslabRterm)
+context("Test term_renviron.")
+#'
+#' ## Set test parameters
+#'
+#+ parameters
+dir <- getwd()
+fn_renviron <- file.path(
+  dir,
+  ".Renviron"
+)
+#'
+#' ## Run test
+#'
+#+ test
+term_renviron(
+  dir = dir,
+  overwrite = TRUE,
+  GITHUB_PAT = "123456",
+  libpath = file.path(dir, "tmp")
+)
+#'
+#' For code coverage.
+#+ covr
+suppressWarnings(
+  term_renviron(
+    overwrite = FALSE
+  )
+)
+#'
+#+ testthat_01, echo=TRUE
+test_that(".Renviron", {
+  expect_true(
+    file.exists(fn_renviron)
+  )
+})
+#'
+#+ testthat_01, echo=TRUE
+test_that("warnings", {
+  expect_warning(
+    term_renviron(
+      dir = dir,
+      overwrite = FALSE,
+      GITHUB_PAT = "123456",
+      libpath = file.path(dir, "tmp")
+    )
+  )
+})
+#'
+#+ cleanup
+unlink(
+  c(
+    fn_renviron,
+    file.path(
+      dir,
+      "tmp"
+    )
+  )
+)
