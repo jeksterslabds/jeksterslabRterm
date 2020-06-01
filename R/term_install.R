25076a0dd26431393be83eb4cf9cabaa3c2af057
Ivan Jacob Agaloos Pesigan <learn.jeksterslab@gmail.com>
2020-06-01 11:50:23 +0800

    Edits to accomodate term_brew, term_pacman, term_apt

--- R/term_install.R
+++ R/term_install.R
@@ -10,7 +10,7 @@
 #' NOTE: `chocolatey` for Windows is not currently implemented.
 #'
 #' @author Ivan Jacob Agaloos Pesigan
-#' @param ... Arguments to pass to [`term_brew`].
+#' @param ... Arguments to pass to [`term_brew()`], or [`term_apt()`].
 #' @inheritParams term_brew
 #' @examples
 #' \dontrun{
@@ -20,7 +20,7 @@
 #' @export
 term_install <- function(packages,
                          ...) {
-  os <- util_distro()
+  os <- util_os()
   if (os == "osx") {
     term_brew(
       packages = packages,
@@ -31,8 +31,29 @@ term_install <- function(packages,
     # chocolatey
   }
   if (os == "linux") {
+    distro <- util_distro()
     # apt
+    if (distro == "debian") {
+      package_manager <- "apt"
+    }
+    if (distro == "ubuntu") {
+      package_manager <- "apt"
+    }
     # pacman
+    if (distro == "arch linux") {
+      package_manager <- "pacman"
+    }
     # yum
+    if (package_manager == "pacman") {
+      term_pacman(
+        packages = packages
+      )
+    }
+    if (package_manager == "apt") {
+      term_apt(
+        packages = packages,
+        ...
+      )
+    }
   }
 }
